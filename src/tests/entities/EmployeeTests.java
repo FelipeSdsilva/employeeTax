aade33b01ab4aa4139752f80573ff689cafc4533
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-11 20:57:54 -0300

    Test for logical encrease of Salary

--- src/tests/entities/EmployeeTests.java
+++ src/tests/entities/EmployeeTests.java
@@ -1,6 +1,23 @@
 package tests.entities;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Employee;
+
 public class EmployeeTests {
 
-	
+	@Test
+	public void salaryShouldEncreaseWhenPercentageIfBiggerZero() {
+		
+		double percentage = 10.0;
+		double salary = 1000.0;
+		double expectedValue = 1100.0;
+		
+		Employee emp = new Employee(1L,"Joaquim","Amaral","Dev",salary);
+		
+		emp.increaseSalary(percentage);
+		
+		Assertions.assertEquals(expectedValue ,emp.getSalary());
+	}
 }
